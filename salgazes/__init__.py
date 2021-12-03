b245b92dfbc033640219980e40838329ce6d9ab5
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2021-12-03 11:03:32 +0200

    Initial setup for packaging

--- salgazes/__init__.py
+++ salgazes/__init__.py
@@ -44,7 +44,3 @@ class CustomLogger:
 from .logmod import logs  # noqa E402
 from . import common  # noqa E402
 from . import settings  # noqa E402
-from .analysis import Analysis  # noqa E402
-from .analysis import Appen  # noqa E402
-from .analysis import Heroku  # noqa E402
-from .analysis import QA  # noqa E402
